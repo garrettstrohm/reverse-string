cf58c794b4b9ff85de9612a31096104837e13ba7
Garrett <garres4@vt.edu>
2022-02-05 12:18:03 -0700

    commit tree 9ace6953bf62a75af48d63cefe8af0f74a251595

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+    let r = "";
+    for(let i = str.length-1; i >= 0; i--){
+      r += str[i];
+    }
+    return r;
 }
 
 if (require.main === module) {
